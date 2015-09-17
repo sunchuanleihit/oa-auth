d5b65fb2738877a46cd068ddc8c3cc06f0e5d636
panhualei <hualei@192.168.0.112>
2015-09-17 19:23:54 +0800

    commit

--- api/src/main/java/com/loukou/auth/api/AuthService.java
+++ api/src/main/java/com/loukou/auth/api/AuthService.java
@@ -5,5 +5,7 @@ import com.loukou.auth.resp.dto.RespDto;
 
 public interface AuthService {
 
-	public RespDto<AuthUserDto> validateToken(String token);
+	public RespDto<AuthUserDto> validateToken(int appId, String token);
+
+	public String login(String userName, String password);
 }
--- api/src/main/java/com/loukou/auth/resp/dto/AuthUserDto.java
+++ api/src/main/java/com/loukou/auth/resp/dto/AuthUserDto.java
@@ -1,6 +1,7 @@
 package com.loukou.auth.resp.dto;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class AuthUserDto implements Serializable {
 
@@ -8,12 +9,22 @@ public class AuthUserDto implements Serializable {
 
 	private int userId;
 
+	private List<String> roles;
+
 	public int getUserId() {
 		return userId;
 	}
 
 	public void setUserId(int userId) {
 		this.userId = userId;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
 	}
 
 }
