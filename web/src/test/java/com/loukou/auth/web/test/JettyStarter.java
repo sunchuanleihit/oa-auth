91cedb163769057277b052fc530411c5a524834c
panhualei <hualei@192.168.0.112>
2015-09-17 23:16:04 +0800

    rename

--- web/src/test/java/com/loukou/auth/web/test/JettyStarter.java
+++ web/src/test/java/com/loukou/auth/web/test/JettyStarter.java
@@ -9,19 +9,17 @@ public class JettyStarter {
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(8088);
 
-        WebAppContext context = new WebAppContext();
-        context.setContextPath("/");
-        context.setDescriptor("src/main/webapps/WEB-INF/web.xml");
-        ResourceCollection resources = new ResourceCollection(new String[] {
-                "src/main/webapps/",
-        		"src/main/resources/"
-            });
-        context.setBaseResource(resources);
-        context.setParentLoaderPriority(true);
-        server.setHandler(context);
+		WebAppContext context = new WebAppContext();
+		context.setContextPath("/");
+		context.setDescriptor("src/main/webapps/WEB-INF/web.xml");
+		ResourceCollection resources = new ResourceCollection(new String[] {
+				"src/main/webapps/", "src/main/resources/" });
+		context.setBaseResource(resources);
+		context.setParentLoaderPriority(true);
+		server.setHandler(context);
 
-        server.start();
-        server.join();
+		server.start();
+		server.join();
 	}
 
 }
