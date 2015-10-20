d904a0f5d1a53064c165eddc0d5fa4835ab345dc
liyunfei <liyunfei@loukou.com>
2015-10-20 09:48:51 +0800

    test

--- auth-lib/src/main/java/com/loukou/auth/core/config/SessionConfigInitializer.java
+++ auth-lib/src/main/java/com/loukou/auth/core/config/SessionConfigInitializer.java
@@ -2,12 +2,12 @@ package com.loukou.auth.core.config;
 
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
-public class SessionConfigInitializer //extends AbstractHttpSessionApplicationInitializer 
+public class SessionConfigInitializer extends AbstractHttpSessionApplicationInitializer 
 {
 
-	/*
+	
 	public SessionConfigInitializer() {
 		super(SessionRedisConfig.class);
 	}
-	*/
+	
 }
