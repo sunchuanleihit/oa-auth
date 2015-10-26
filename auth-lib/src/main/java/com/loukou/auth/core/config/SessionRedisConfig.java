ca01c909bda7a9067c2a812d2fe46aacc010d9a3
nonggia <nonggia@163.com>
2015-10-26 17:19:59 +0800

    rm sessionconfig

--- auth-lib/src/main/java/com/loukou/auth/core/config/SessionRedisConfig.java
+++ auth-lib/src/main/java/com/loukou/auth/core/config/SessionRedisConfig.java
@@ -1,26 +1,26 @@
 package com.loukou.auth.core.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import com.github.diamond.client.PropertiesConfigurationFactoryBean;
 
-@EnableRedisHttpSession
+//@EnableRedisHttpSession
 public class SessionRedisConfig {
 
 	private static final String KEY_REDIS_PORT = "redis.port";
 	private static final String KEY_REDIS_HOST = "redis.host";
 
-	@Bean
-	public JedisConnectionFactory connectionFactory() {
-		JedisConnectionFactory connection = new JedisConnectionFactory();
-		int port = PropertiesConfigurationFactoryBean
-				.getPropertiesConfiguration().getInt(KEY_REDIS_PORT);
-		String host = PropertiesConfigurationFactoryBean
-				.getPropertiesConfiguration().getString(KEY_REDIS_HOST);
-		connection.setPort(port);
-		connection.setHostName(host);
-		return connection;
-	}
+//	@Bean
+//	public JedisConnectionFactory connectionFactory() {
+//		JedisConnectionFactory connection = new JedisConnectionFactory();
+//		int port = PropertiesConfigurationFactoryBean
+//				.getPropertiesConfiguration().getInt(KEY_REDIS_PORT);
+//		String host = PropertiesConfigurationFactoryBean
+//				.getPropertiesConfiguration().getString(KEY_REDIS_HOST);
+//		connection.setPort(port);
+//		connection.setHostName(host);
+//		return connection;
+//	}
 }
